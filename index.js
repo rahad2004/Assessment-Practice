662402150ebc3089407df2807f843d2a7836de56
rahad2004 <rahadmondal165@gmail.com>
2025-05-31 23:26:43 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,5 @@
+// Mobile menu toggle
+document.getElementById("menu-toggle").addEventListener("click", function () {
+  const menu = document.getElementById("mobile-menu");
+  menu.classList.toggle("hidden");
+});
